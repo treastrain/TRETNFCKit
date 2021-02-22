4bfc9d0dc3abc18b1c9850e4e649e0eb678b1163
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-02-23 04:17:11 +0900

    Create FeliCaPollingResponse.swift

---
+++ Sources/NFCKit/Structures/FeliCaPollingResponse.swift
@@ -0,0 +1,33 @@
+//
+//  FeliCaPollingResponse.swift
+//  
+//
+//  Created by treastrain on 2021/02/19.
+//
+
+import Foundation
+#if os(iOS)
+import CoreNFC
+#endif
+
+/// Response from Polling command.
+public struct FeliCaPollingResponse {
+    public init(manufactureParameter: Data, requestData: Data?) {
+        self.manufactureParameter = manufactureParameter
+        self.requestData = requestData
+    }
+    
+    #if os(iOS) && !targetEnvironment(macCatalyst)
+    @available(iOS 14.0, *)
+    public init(from coreNFCInstance: CoreNFC.NFCFeliCaPollingResponse) {
+        self.manufactureParameter = coreNFCInstance.manufactureParameter
+        self.requestData = coreNFCInstance.requestData
+    }
+    #endif
+    
+    /// PMm
+    public var manufactureParameter: Data
+
+    /// Data requested by the Request Code
+    public var requestData: Data?
+}
