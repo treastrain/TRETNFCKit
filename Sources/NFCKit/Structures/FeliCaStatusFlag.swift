6e845881016b91ed5b8ca205a290a3f1a876c4e3
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-03-02 22:02:39 +0900

    Create FeliCaStatusFlag.swift

---
+++ Sources/NFCKit/Structures/FeliCaStatusFlag.swift
@@ -0,0 +1,33 @@
+//
+//  FeliCaStatusFlag.swift
+//  
+//
+//  Created by treastrain on 2021/03/02.
+//
+
+import Foundation
+#if os(iOS)
+import CoreNFC
+#endif
+
+/// Response status flags.
+public struct FeliCaStatusFlag {
+    public init(statusFlag1: Int, statusFlag2: Int) {
+        self.statusFlag1 = statusFlag1
+        self.statusFlag2 = statusFlag2
+    }
+    
+    #if os(iOS) && !targetEnvironment(macCatalyst)
+    @available(iOS 14.0, *)
+    public init(from coreNFCInstance: CoreNFC.NFCFeliCaStatusFlag) {
+        self.statusFlag1 = coreNFCInstance.statusFlag1
+        self.statusFlag2 = coreNFCInstance.statusFlag2
+    }
+    #endif
+    
+    /// Status flag 1.
+    public var statusFlag1: Int
+    
+    /// Status flag 2.
+    public var statusFlag2: Int
+}
