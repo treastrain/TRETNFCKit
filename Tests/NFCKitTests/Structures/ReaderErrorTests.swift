f84d957e31b967cf8fc511e85fc7ec3797440ff2
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-04-30 00:27:14 +0900

    Core NFC global variables Support

--- Tests/NFCKitTests/Structures/ReaderErrorTests.swift
+++ Tests/NFCKitTests/Structures/ReaderErrorTests.swift
@@ -34,4 +34,21 @@ final class ReaderErrorTests: XCTestCase, NFCKitTests {
         _ = ReaderError(.kitErrorCoreNFCAndNFCKitErrorCodeConversionFailed, userInfo: [:], description: "")
         #endif
     }
+    
+    func testReaderGlobalVariables() throws {
+        #if os(iOS) && !targetEnvironment(macCatalyst)
+        guard #available(iOS 13.0, *) else {
+            XCTFail("There is a problem with the OS version you are testing.")
+            return
+        }
+        
+        XCTAssertEqual(CoreNFC.NFCErrorDomain.replacingOccurrences(of: "NFC", with: "TRETNFCKit"), ErrorDomain)
+        XCTAssertEqual(CoreNFC.NFCISO15693TagResponseErrorKey, ISO15693TagResponseErrorKey)
+        XCTAssertEqual(CoreNFC.NFCTagResponseUnexpectedLengthErrorKey, TagResponseUnexpectedLengthErrorKey)
+        #else
+        _ = ErrorDomain
+        _ = ISO15693TagResponseErrorKey
+        _ = TagResponseUnexpectedLengthErrorKey
+        #endif
+    }
 }
